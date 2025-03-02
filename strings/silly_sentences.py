47ab9a8b025b85cb467953a4b1746acfd942c75e
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-03-01 20:46:36 -0700

    Update silly_sentences.py

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,7 +1,7 @@
-# josy Ramirez, silly sentences python
+# Josy Ramirez, silly sentences python
 
-animal= input ("Name an animal")
-place = input ("Name a place ")
-verb = input ("Name a verb")
+animal = input("Name an animal: ")
+place = input("Name a place: ")
+verb = input("Name a verb: ")
 
-print("Whith their friend the loud laughing donkey")
+print(f"The {animal} went to the {place} and it {verb} with their friend, the loud laughing donkey.")
