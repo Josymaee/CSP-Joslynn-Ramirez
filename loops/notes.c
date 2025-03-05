3ea216b7454bc45d405057dff4b2ea76d5192594
Documents <joslynn.ramirez@ucas-edu.net>
2025-03-05 11:48:37 -0700

    bru

--- loops/notes.c
+++ loops/notes.c
@@ -37,9 +37,30 @@ int size = sizeof(grades);
 // length in list items
 int length = sizeof(grades)/sizeof(grades[0]);
 printf("%d\n", length);
+// array with strings
+// First brackets set the length of the list
+//seconds brackets sets the length of each string 
+char movies[][20] = {"Cars", "Trausre Planet", "An American Tale", "Marley and m=Me", "The Avengers"};
+printf("The movie is %s!\n", movies[1]);
+int mlength = sizeof(movies)/sizeof(movies[0])
 // 9. How do you make loops for C
 
+// set the iterator, keeps track of how many times ive gone through the loop (best practice to set at x or i)
+int x;
+// in parenthesis (Starting point; ending point; what we count by)
+for(x = 0; x <= 10; x+=2){
+    printf("%d\n", x);
+}
+int m;
+for(m=0;m<mlength;m++){
+    printf("%s\n", movies[m]);
+}
 // 10. How do you make while loops in C
+int w = 0;
 
+while(w<100){
+    printf("%d\n", w);
+    w += 10;
+}
     return 0;
 }
