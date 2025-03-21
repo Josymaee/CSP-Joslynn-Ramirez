8c4f2810484ba97b0c7df9e66efb62e9446557d8
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-03-20 22:01:23 -0600

    FizzBuzz.py

--- loops/FizzBuzz.py
+++ loops/FizzBuzz.py
@@ -1,1 +1,11 @@
 # Joslynn Ramirez, fizz buzz python 
+
+for num in range(1, 51):  
+    if num % 3 == 0 and num % 5 == 0:  
+        print("FizzBuzz")  
+    elif num % 3 == 0:  
+        print("Fizz")  
+    elif num % 5 == 0:  
+        print("Buzz")  
+    else:  
+        print(num)  
