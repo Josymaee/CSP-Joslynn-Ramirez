ff8862c9710d33674dec9c14ff43a2396e3f11b9
Documents <joslynn.ramirez@ucas-edu.net>
2025-03-25 11:22:22 -0600

    fizzbuyss

---
+++ PROJECT/project.py
@@ -0,0 +1,1 @@
+# Joslynn R
--- loops/FizzBuzz.c
+++ loops/FizzBuzz.c
@@ -1,8 +1,21 @@
 // Joslynn Ramirez, Fizz Buzz C
 
-#include <stdio.h>
-
-int main(void){
-        printf("Hello world");
-        return 0;
-}
+#include <stdio.h>  
+  
+int main(void) {  
+    for (int num = 1; num <= 50; num++) {    
+        if (num % 3 == 0 && num % 5 == 0) {    
+            printf("FizzBuzz\n");  
+        }   
+        else if (num % 3 == 0) {    
+            printf("Fizz\n");    
+        }   
+        else if (num % 5 == 0) {    
+            printf("Buzz\n");    
+        }   
+        else {    
+            printf("%d\n", num);   
+        }  
+    }  
+    return 0;  
+}  
