98e5e286e46b6a37964a49d784dfb9511f6c5f02
josymaee <joslynn.ramirez@ucas-edu.net>
2025-04-22 11:48:12 -0600

    idk

---
+++ javascript/notes.js
@@ -0,0 +1,25 @@
+let images = ["https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU","https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"]
+let counter = 0
+function change (){
+    if (counter < images.length){
+    document.getElementById("img").
+    src = images[counter]
+    counter += 1
+    }else{
+        counter = 0
+        docuement.getElementById ("img").src = images
+        [counter]
+    }
+}
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World!"
+}
+function hover(){
+    document.getElementById("img").srx = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Cape_may.jpg/500px-Cape_may.jpg"
+}
+function leave(){
+    document.getElementById("img").srx = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQscYIB3DXqEvjTi5mV_ldX6p1zQ1LvAnYE9bB3SkgzKPFI1MjSC2nfgbRfKUW_ANS7TJ0&usqp=CAU"
+}
+function hidden(){
+    document.getElementById ("oregon").style.display = "block"
+}
