1a9336e252ba4e78542ff9c2aa18e4e79c195cbf
josymaee <joslynn.ramirez@ucas-edu.net>
2025-04-25 11:49:01 -0600

    notesagain

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,5 +1,24 @@
+//javascript notes
+
+function show(){
+    document.getElementById("oregon").style.display = "block"
+
+}
+
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World!"
+}
+function enter(){
+    document.getElementById("btn").style.color = "white"
+    document.getElementById("btn").style.backgroundColor="green"
+}
+function down(){
+    document.getElementById("btn").style.backgroundcolor= "red"
+}
+
 let images = ["https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU","https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"]
 let counter = 0
+
 function change (){
     if (counter < images.length){
     document.getElementById("img").
