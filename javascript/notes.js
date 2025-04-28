ca4a8e03a3b508694605405673b5ead09668433d
josymaee <joslynn.ramirez@ucas-edu.net>
2025-04-28 11:34:56 -0600

    jijfos

--- javascript/notes.js
+++ javascript/notes.js
@@ -2,20 +2,24 @@
 
 function show(){
     document.getElementById("oregon").style.display = "block"
-
 }
 
 function hello(){
-    document.getElementById("title").innerHTML = "Hello World!"
+    let name = prompt("What is your name?", "Josy")
+    document.getElementById("title").innerHTML= "Hello " + name + "!"
 }
+
 function enter(){
     document.getElementById("btn").style.color = "white"
     document.getElementById("btn").style.backgroundColor="green"
 }
 function down(){
-    document.getElementById("btn").style.backgroundcolor= "red"
+    document.getElementById("btn").style.backgroundColor= "red"
 }
-
+function out(){
+    document.getElementById("btn").style.color= "black"
+    document.getElementById("btn").style.backgroundColor="lightgrey"
+}
 let images = ["https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU","https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"]
 let counter = 0
 
@@ -30,33 +34,7 @@ function change (){
         [counter]
     }
 }
-function hello(){
-    let name = window.prompt("What is your name?", "Josy")
-    document.getElementById("title").innerHTML = "Hello!" + name + "!"
-}
-function hover(){
-    document.getElementById("img").srx = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Cape_may.jpg/500px-Cape_may.jpg"
-}
-function leave(){
-    document.getElementById("img").srx = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQscYIB3DXqEvjTi5mV_ldX6p1zQ1LvAnYE9bB3SkgzKPFI1MjSC2nfgbRfKUW_ANS7TJ0&usqp=CAU"
-}
-function hidden(){
-    document.getElementById ("oregon").style.display = "block"
-}
-function pop(){
-    window.alert("Really don't click that!")
-}
-function show (){
-    alert
-document.getElementById("lost"). style.display
-} 
 
-function view(){
-    if(document.getElementById("more").style.display==="block"){
-    document.getElementById("more").style.display = "none"
-    document.getElementById("shw").style.display = "Show more"
-    }else{
-        document.getElementById("more").style.display = "block"
-        document.getElementById("shw").style.display = "Show less"
-    }
+function popup(){
+    window.alert("Don't click this. Realy!")
 }
