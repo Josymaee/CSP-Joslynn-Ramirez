e704de008d013f71cb351af27e2f73c06626d518
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-04-30 21:29:26 -0600

    Update notes.js

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,40 +1,43 @@
-//javascript notes
+// JavaScript Notes
 
-function show(){
-    document.getElementById("sunset").style.display = "block"
-}
-
-function hello(){
-    let name = prompt("What is your name?", "Josy")
-    document.getElementById("title").innerHTML= "Hello " + name + "!"
+function show() {
+  document.getElementById("oregon").style.display = "block"; // Make sure the HTML has id="oregon"
 }
 
-function enter(){
-    document.getElementById("btn").style.color = "white"
-    document.getElementById("btn").style.backgroundColor="green"
-}
-function down(){
-    document.getElementById("btn").style.backgroundColor= "red"
-}
-function out(){
-    document.getElementById("btn").style.color= "black"
-    document.getElementById("btn").style.backgroundColor="lightgrey"
-}
-let images = ["https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU","https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"]
-let counter = 0
-
-function change (){
-    if (counter < images.length){
-    document.getElementById("img").
-    src = images[counter]
-    counter += 1
-    }else{
-        counter = 0
-        docuement.getElementById ("img").src = images
-        [counter]
-    }
+function hello() {
+  let name = prompt("What is your name?", "Josy");
+  document.getElementById("title").innerHTML = "Hello " + name + "!";
 }
 
-function popup(){
-    window.alert("Don't click this. Really!")
+function enter() {
+  const btn = document.getElementById("btn");
+  btn.style.color = "white";
+  btn.style.backgroundColor = "green";
+}
+
+function down() {
+  document.getElementById("btn").style.backgroundColor = "red";
+}
+
+function out() {
+  const btn = document.getElementById("btn");
+  btn.style.color = "black";
+  btn.style.backgroundColor = "lightgrey";
+}
+
+let images = [
+  "https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU",
+  "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"
+];
+let counter = 0;
+
+function change() {
+  const img = document.getElementById("image"); // Make sure HTML uses id="image"
+  img.src = images[counter];
+  counter = (counter + 1) % images.length; // Loop back to 0 when done
+}
+
+function popup() {
+  window.alert("Don't click this. Really!");
 }
