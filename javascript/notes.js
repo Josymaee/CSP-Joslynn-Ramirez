3bcf9770d5a93101438aeace6bcebffc57cb1414
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-05-04 20:21:16 -0600

    Update notes.js

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,42 +1,43 @@
 // JavaScript Notes
 
-function show() {
-  document.getElementById("oregon").style.display = "block";
+document = notes.html
+function hello(){
+    let name = window.prompt("what is your name?", "you better type your name")
+    document.getElementById("title").
+    innerHTML = "hello " + name + "!"
+}
+function change(){
+    document.getElementById("img").src = "https://utahhikingbeauty.com/wp-content/uploads/2020/07/Mount-Timpanogos-Aspen-Grove-Waterfall-Emerald-Lake-1.jpg"
 }
 
-function hello() {
-  let name = prompt("What is your name?", "Josy");
-  document.getElementById("title").innerHTML = "Hello " + name + "!";
+function highlight(){
+    document.getElementById("btn").style.backgroundColor ="orange"
+     document.getElementById("btn").style.color ="white"
+}
+function normal(){
+     document.getElementById("btn").style.backgroundColor ="gray"
+     document.getElementById("btn").style.color ="black"
+}
+function push(){
+     document.getElementById("btn").style.backgroundColor ="red"
+
+}
+function show(){
+    document.getElementById("hidden").style.display = "block"
+}
+function pop(){
+    window.alert("im omniing it with omni dih!")
+}
+function more(){
+    if(document.getElementById("extra").style.display != "flex")
+        {document.getElementById("extra").style.display = "flex"
+    document.getElementById("shw").innerHTML = "show less"
+
+    }else{
+        document.getElementById("extra").style.display = "none"
+    document.getElementById("shw").innerHTML = "show more"
+    }
 }
 
-function enter() {
-  const btn = document.getElementById("btn");
-  btn.style.color = "white";
-  btn.style.backgroundColor = "green";
-}
-
-function down() {
-  document.getElementById("btn").style.backgroundColor = "red";
-}
-
-function out() {
-  const btn = document.getElementById("btn");
-  btn.style.color = "black";
-  btn.style.backgroundColor = "lightgrey";
-}
-
-let images = [
-  "https://gardenandgun.com/wp-content/uploads/2024/02/Atlantic-Beach_Terry-Ward.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQYrHIgUGNc8-i5zOU6CnX-IR_d9x0esyBoBfM6CSctg9QTPyita5GEfFzjdok5Vr-hKQ&usqp=CAU",
-  "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1824,w_2736,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Oregon-best-beaches-summer-cape-kiwanda_2489076057_wzlhja.jpg"
-];
-let counter = 0;
-
-function change() {
-  const img = document.getElementById("image");
-  img.src = images[counter];
-  counter = (counter + 1) % images.length;
-}
-function popup() {
-  window.alert("Don't click this. Really!");
-}
+<p id = "on">3 more sentences abt why its the best and things it has to offer(lead into image two) hkgifqwhggfoihewf wgihfiwhgfdoiuwqehf gwiohqoirgfhiwqeuf gqifiwqehfiu3</p>
+    <img id = "on2" src = "hidden image"></img>
