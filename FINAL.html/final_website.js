a95c303d3c9b15c3e3d5f4f00303110692843dd5
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-05-07 22:31:41 -0600

    Update final_website.js

--- FINAL.html/final_website.js
+++ FINAL.html/final_website.js
@@ -9,4 +9,11 @@
     <img id="epicthings" src="https://thaka.bing.com/th/id/OIP.vqkOhZkItCmwKA9MUtax-wHaDH?w=327&h=147&c=7&r=0&o=5&pid=1.7" height="500px">
 </div>
 
+function change() {
+    document.getElementById("img1").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfvA0FFWDQ6P0dVed5CQfFUbV91-LzeG8aT8wQHG67tVqTjB_-GQ&s=10&ec=72940545";
+}
+
+function hello() {
+    document.getElementById("on").style.display = "flex";
+}
 
