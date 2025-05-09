92c340505e577ed869aa666326d8c460eb94e2c0
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-05-08 23:36:11 -0600

    Update final_website.js

--- FINAL.html/final_website.js
+++ FINAL.html/final_website.js
@@ -1,19 +1,16 @@
-
-<img id="img1" class="capitol" src="https://example.com/initial-image.jpg" height="150px" onmouseover="change()" />
-
-
-<button id="btn" onclick="hello()">Click here to view more information</button>
-
-<div id="on" style="display: none;">
-    <p id="hiddenpara">This is more information about Capitol Reef National Park.</p>
-    <img id="epicthings" src="https://thaka.bing.com/th/id/OIP.vqkOhZkItCmwKA9MUtax-wHaDH?w=327&h=147&c=7&r=0&o=5&pid=1.7" height="500px">
-</div>
-
 function change() {
-    document.getElementById("img1").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfvA0FFWDQ6P0dVed5CQfFUbV91-LzeG8aT8wQHG67tVqTjB_-GQ&s=10&ec=72940545";
+    // Change image on hover
+    document.getElementById("img1").src =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfvA0FFWDQ6P0dVed5CQfFUbV91-LzeG8aT8wQHG67tVqTjB_-GQ&s=10&ec=72940545";
 }
 
-function hello() {
-    document.getElementById("on").style.display = "flex";
-}
+function toggleInfo() {
+    const infoBox = document.getElementById("on");
 
+    // Toggle with fade effect
+    if (infoBox.classList.contains("show")) {
+        infoBox.classList.remove("show");
+    } else {
+        infoBox.classList.add("show");
+    }
+}
