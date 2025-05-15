0cb61fa8946cb40bb342f33ee60a5c816755dd2b
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-05-14 21:24:57 -0600

    Update final_website.js

--- FINAL.html/final_website.js
+++ FINAL.html/final_website.js
@@ -1,16 +1,4 @@
-function change() {
-    // Change image on hover
-    document.getElementById("img1").src =
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfvA0FFWDQ6P0dVed5CQfFUbV91-LzeG8aT8wQHG67tVqTjB_-GQ&s=10&ec=72940545";
-}
-
 function toggleInfo() {
-    const infoBox = document.getElementById("on");
-
-    // Toggle with fade effect
-    if (infoBox.classList.contains("show")) {
-        infoBox.classList.remove("show");
-    } else {
-        infoBox.classList.add("show");
-    }
+    const info = document.getElementById("on");
+    info.style.display = info.style.display === "none" ? "block" : "none";
 }
